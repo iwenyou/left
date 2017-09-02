789fca5dcb37d377450ef1e7a55f42c03f958c6d
Wen Zhang <iwenyou@gmail.com>
2017-09-01 19:05:21 -0700

    dependence installed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route,Switch } from 'react-router.dom';
+import promise from 'redux-promise';
+
 import logo from '../logo.svg';
 import '../App.css';
+
 
 class App extends Component {
   render() {
