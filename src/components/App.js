e30c9c040603643e2bd9289ee777b6eda902bab6
Wen Zhang <iwenyou@gmail.com>
2017-09-13 11:14:55 -0700

    changed  about me page

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import '../App.css';
-
 
 class App extends Component {
   render() {
     return (
       <div className="nav_bar">
         <nav className="navbar navbar-default">
-          <div className="container">
+          <div className="container-fluid">
             <div className="navbar-header">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
               <a className="navbar-brand col-xs-offset-3" href="/">Time Story</a>
             </div>
-            <div className="collapse navbar-collapse">
-              <ul className="nav navbar-nav navbar-right">
-                <li><a href="/about">About Me</a></li>
+            <div className="collapse navbar-collapse" id="myNavbar">
+              <ul className="nav navbar-nav">
+                <li>
+                  <a href="/about">About Me</a>
+                </li>
               </ul>
             </div>
           </div>
